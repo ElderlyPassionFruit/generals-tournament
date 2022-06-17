0304153f2b3afabae03b97cb96c9a28ac6139c42
Igor Markelov <0987654321lion.zz@gmail.com>
2022-06-18 02:00:48 +0300

    Add 5*10 map, two stupid strategies

--- examples/afk_strategy.cpp
+++ examples/afk_strategy.cpp
@@ -1,5 +1,73 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+enum CellType{
+    UNKNOWN,
+    EMPTY,
+    CITY,
+    CAPITAL,
+    MOUNTAINS
+};
+
+struct Cell{
+    int visible;
+    CellType type;
+    int owner;
+    int army_size;
+};
+
+struct Statistics{
+    int total_army;
+    int total_fields;
+};
+
+void output_afk() {
+    // cerr << "here!!!!" << endl;
+    cout << -1 << endl;
+}
+
+void output_move(int type, int x1, int y1, int x2, int y2) {
+    cout << type << " " << x1 + 1 << " " << y1 + 1 << " " << x2 + 1 << " " << y2 + 1 << endl;
+}
+
+int n, m, k, id;
+vector<Statistics> stat;
+vector<vector<Cell>> info;    
+
+void input() {
+    // cerr << "start input strategy" << std::endl;
+    int alive;
+    cin >> alive;
+    // cerr << "alive = " << alive << std::endl;
+    if (!alive) {
+        exit(0);
+    }
+    for (auto& [total_army, total_fields] : stat) {
+        cin >> total_army >> total_fields;
+        // cerr << "total_army = " << total_army << " total_fields = " << total_fields << std::endl;
+    }
+    for (int x = 0; x < n; ++x) {
+        for (int y = 0; y < m; ++y) {
+            auto& cell = info[x][y];
+            int value;
+            cin >> cell.visible >> value;
+            cell.type = static_cast<CellType>(value);
+            if (cell.visible && cell.type != MOUNTAINS) {
+                cin >> cell.owner >> cell.army_size;
+            }
+        }
+    }
+    // cerr << "finish input" << std::endl;
+}
 
 int main() {
-    
+    cin >> n >> m >> k >> id;
+    stat.resize(k);
+    info.resize(n, vector<Cell>(m));   
+    while (true) {
+        input();
+        output_afk();
+    }
 }
